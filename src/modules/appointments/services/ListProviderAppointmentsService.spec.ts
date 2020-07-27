70a0bb34402431745541cd2044165eda39dc1ce8
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-07-27 14:44:50 -0300

    evolving backend

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,40 @@
+// // import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+let listProviderAppointments: ListProviderAppointmentsService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviders', () => {
+    beforeEach(() => {
+        fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        listProviderAppointments = new ListProviderAppointmentsService(fakeAppointmentsRepository);
+    });
+
+    it("should be able to list all the provider's appointments in day", async () => {
+        const appointment1 = await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            user_id: 'Gerso',
+            date: new Date(2020, 4, 20, 8, 0, 0),
+        });
+        const appointment2 = await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            user_id: 'Gerso',
+            date: new Date(2020, 4, 20, 9, 0, 0),
+        });
+        const appointment3 = await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            user_id: 'Gerso',
+            date: new Date(2020, 4, 20, 10, 0, 0),
+        });
+
+        const availability = await listProviderAppointments.execute({
+            provider_id: 'user',
+            year: 2020,
+            month: 5,
+            day: 20,
+        });
+
+        expect(availability).toEqual([appointment1, appointment2, appointment3]);
+    });
+});
