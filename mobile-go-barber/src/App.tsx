3d7a52ffc9d861e242af5eae7463eec248c11632
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-09-04 16:42:24 -0300

    deploying

--- mobile-go-barber/src/App.tsx
+++ mobile-go-barber/src/App.tsx
@@ -1,19 +1,25 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-    <NavigationContainer>
-        <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-        <AppProvider>
-            <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-                <Routes />
-            </View>
-        </AppProvider>
-    </NavigationContainer>
-);
+const App: React.FC = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+    return (
+        <NavigationContainer>
+            <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+            <AppProvider>
+                <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+                    <Routes />
+                </View>
+            </AppProvider>
+        </NavigationContainer>
+    );
+};
 export default App;
--- mobile-go-barber/src/services/api.ts
+++ mobile-go-barber/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.2.105:3333',
+    // baseURL: 'http://192.168.2.105:3333',
+    baseURL: 'https://nodedeploy.fullmindstudio.net/',
 });
 
 export default api;
